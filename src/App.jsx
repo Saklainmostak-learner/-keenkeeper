de7cacecb55e95725364b3b5b1fae7afef8045b9
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-04-14 03:09:56 +0600

    File Create

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-6xl font-bold bg-red-500 text-white p-10'> This is website</h1>
+    </>
+  )
+}
+
+export default App
