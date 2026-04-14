03d94a7ee22271df25579838cb67af788c4869a9
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-04-15 02:49:41 +0600

    Herosection add

--- src/components/CardSummary.jsx
+++ src/components/CardSummary.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const CardSummary = () => {
+const CardSummary = ({value,label}) => {
     return (
-        <div>
-            
+        <div className='bg-[#FFFFFF] px-6 py-8 text-center shadow-sm ring-1 ring-slate-200/70 rounded-sm'>
+            <h3 className='text-4xl font-bold text-[#244D3F] md:text-5xl'>{value}</h3>
+            <p className='mt-3 text-[#64748B] font-medium'>{label}</p>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import Banner from '../components/Banner';
+import React, { useEffect, useState } from "react";
+import Banner from "../components/Banner";
+import CardSummary from "../components/CardSummary";
 
 const Home = () => {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch("/friends.json")
+      .then((res) => res.json())
+      .then((data) => setFriends(data));
+  }, []);
+  const totalFriends=friends.length;
+  const onTrack=friends.filter(
+    (friend)=>friend.status==="on-track").length;
+    const needAttention=friends.filter(
+      (friend)=>friend.status==="overdue"|| friend.status==="almost due").length;
+    
+  
+const interactionsThisMonth=12;
   return (
-    <section>
-      <div>
-        <Banner/>
+    <section className="px-4 py-8 md:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl">
+        <Banner />
+        <div className="-mt-6 grid gap-5 md:grid-cols-2 xl:grid-cols-4">
+          <CardSummary value={totalFriends} label="Total Friends" />
+          <CardSummary value={onTrack} label="On Track" />
+          <CardSummary value={needAttention} label="Need Attention" />
+          <CardSummary
+            value={interactionsThisMonth}
+            label="Interactions This Month"
+          />
+        </div>
       </div>
     </section>
   );
