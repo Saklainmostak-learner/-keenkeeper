86d46244762510e27240ee494a076c39f4035a5b
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-04-15 04:48:47 +0600

    Friend card add

---
+++ src/components/FriendsCard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const statusColors = {
+  overdue: "bg-[#EF4444] text-white",
+  "almost due": "bg-[#EFAD44] text-white",
+  "on-track": "bg-[#244D3F] text-white",
+};
+
+const FriendsCard = ({ friend }) => {
+  const { id, name, picture, days_since_contact, tags, status } = friend;
+
+  return (
+    <Link to={`/friend/${id}`} className="block h-full">
+      <div className="h-full min-h-63 bg-[#FFFFFF] px-6 py-8 shadow-sm ring-1 ring-slate-200/70 transition duration-200 rounded-sm flex flex-col text-center hover:-translate-y-1 hover:shadow-md">
+        <img
+          src={picture}
+          alt={name}
+          className="mx-auto h-16 w-16 rounded-full object-cover md:h-20 md:w-20"
+        />
+        <h3 className="mt-5 text-3xl font-bold leading-none text-[#1F2937]">
+          {name}
+        </h3>
+        <p className="mt-3 text-sm font-medium text-[#64748B]">
+          {days_since_contact}d ago
+        </p>
+        <div className="mt-4 flex  flex-wrap items-center min-h-12      justify-center gap-2">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="rounded-full bg-[#CBFADB] px-3 py-1 text-xs font-medium text-[#244D3F] uppercase"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className="mt-3">
+          <span
+            className={`inline-block rounded-full px-4 py-1 text-xs font-semibold ${statusColors[status]}`}
+          >
+            {status === "almost due"
+              ? "Almost Due"
+              : status === "on-track"
+                ? "On-Track"
+                : "Overdue"}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default FriendsCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import CardSummary from "../components/CardSummary";
+import FriendsCard from "../components/FriendsCard";
 
 const Home = () => {
   const [friends, setFriends] = useState([]);
@@ -29,6 +30,16 @@ const interactionsThisMonth=12;
             value={interactionsThisMonth}
             label="Interactions This Month"
           />
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="mb-6">
+          <h2 className="text-3xl font-bold text-[#1F2937]">Your Friends</h2>
+        </div>
+        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {friends.map((friend)=>(
+            <FriendsCard key={friend.id} friend={friend}/>
+          ))}
         </div>
       </div>
     </section>
